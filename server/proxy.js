a7ee2122b0d2658ecb39891c7f858da539f95904
Mark Breznik <mark.breznik@gmail.com>
2022-10-30 09:50:16 +0100

    Removed node_modules and added to gitignore

--- server/proxy.js
+++ server/proxy.js
@@ -2,9 +2,9 @@ import httpProxy from "http-proxy";
 
 export const proxy = httpProxy.createProxyServer({
   /**
-   * Get the actual bac-end service url from env variables.
+   * Get the actual back-end service url from env variables.
    * We shouldn't prefix the env variable with NEXT_PUBLIC_* to avoid exposing it to the client.
    */
-  target: "http://89.142.196.64:3000/",
+  target: "http://localhost:3000/",
   autoRewrite: false,
 });
